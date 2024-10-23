1a6f9ccae8257283cc07cded127feecc9b561cff
Ksenia <kseniash157@gmail.com>
2024-10-23 22:58:52 +0300

    commit

---
+++ main.js
@@ -0,0 +1,95 @@
+/* MAP BUTTONS */
+var HIDDEN_CLASS_NAME = 'hidden'
+var TARGET_CLASS_NAME = 'target'
+var SOURCE_CLASS_NAME = 'source'
+
+var targetIdToShow = 1
+
+function main() {
+	var targets = getElements(TARGET_CLASS_NAME)
+	var sources = getElements(SOURCE_CLASS_NAME)
+	sources.forEach(function (sourceNode) {
+		var sourceNodeId = extractId(sourceNode, SOURCE_CLASS_NAME)
+		sourceNode.addEventListener('click', function () {
+			showTarget(targets, sourceNodeId)
+		})
+	})
+	showTarget(targets, targetIdToShow)
+}
+
+function getElements(type) {
+	return [].slice.call(document.querySelectorAll('.' + type)).sort(function (targetNode1, targetNode2) {
+		var target1Num = extractId(targetNode1, TARGET_CLASS_NAME)
+		var target2Num = extractId(targetNode2, TARGET_CLASS_NAME)
+		return target1Num > target2Num
+	})
+}
+
+function extractId(targetNode, baseClass) {
+	var currentClassIndex = targetNode.classList.length
+	while (currentClassIndex--) {
+		var currentClass = targetNode.classList.item(currentClassIndex)
+		var maybeIdNum = parseInt(currentClass.split('-')[1])
+		if (isNaN(maybeIdNum)) {
+			continue
+		}
+		var classStrinToValidate = baseClass + '-' + maybeIdNum
+		if (classStrinToValidate === currentClass) {
+			return maybeIdNum
+		}
+	}
+}
+
+function showTarget(targets, targetId) {
+	targets.forEach(function (targetNode, targetIndex) {
+    var currentTargetNodeId = extractId(targetNode, TARGET_CLASS_NAME)
+		if (currentTargetNodeId === targetId) {
+			targetNode.classList.remove(HIDDEN_CLASS_NAME)
+		} else {
+			targetNode.classList.add(HIDDEN_CLASS_NAME)
+		}
+	})
+}
+
+main()
+
+
+/* REVIEWS */
+
+// Получаем элементы слайдера
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('.slider__item'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+// Инициализация слайдера
+updateSlider();
